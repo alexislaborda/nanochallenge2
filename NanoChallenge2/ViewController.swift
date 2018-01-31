027b099daba7c12299cfb0d216105b876ae4b127
Alexis Laborda <alexislaborda@outlook.com.br>
2018-01-31 21:42:22 -0200

    InitialCommit

---
+++ NanoChallenge2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NanoChallenge2
+//
+//  Created by Alexis Ruben Garcias Laborda on 31/01/2018.
+//  Copyright © 2018 Alexis Laborda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
